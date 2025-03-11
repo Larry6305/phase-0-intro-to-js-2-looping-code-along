e882ae6f84470504bc768c8c56c4b8641aa5e47a
Larry <melarry46@gmail.com>
2025-03-11 10:28:02 +0300

    copmlete looping code lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+// Function to return thank you messages for each name
+function writeCards(names, event) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`);
+  }
+  
+  // Function to count down from a number to 0 and log each number
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  module.exports = { writeCards, countDown };
+  
